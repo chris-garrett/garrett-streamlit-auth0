99f7366b4115ece9aad49bd5c8fbfb4ac8e4df51
Chris Garrett <chris@nesteggs.ca>
2023-08-10 20:13:53 -0600

    fix: [17] button state when error is raised, add to example

--- examples/multi-page/pages/02_PageTwo.py
+++ examples/multi-page/pages/02_PageTwo.py
@@ -4,13 +4,19 @@ import streamlit as st
 from auth0_component import login_button
 
 st.write("Page 2")
-auth = login_button(
-    clientId=os.getenv("clientId"),
-    domain=os.getenv("domain"),
-    audience=os.getenv("audience"),
-    issuer=os.getenv("issuer"),
-    debug_logs=os.getenv("debug_logs")
-)
+
+try:
+    auth = login_button(
+        clientId=os.getenv("clientId"),
+        domain=os.getenv("domain"),
+        audience=os.getenv("audience"),
+        issuer=os.getenv("issuer"),
+        debug_logs=os.getenv("debug_logs"),
+    )
+except Exception as e:
+    st.write(f"Auth failed {type(e)} {e}")
+    auth = None
+
 if auth:
     st.write(auth)
     st.balloons()
